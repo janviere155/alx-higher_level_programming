23ebd3fe028302cb08f862ed35bb03181dad1f26
janviere155 <janviere155@gmail.com>
2022-12-01 10:38:40 -0800

    modules_import: hidden file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,9 @@
 import hidden_4
 
 if __name__ == '__main__':if __name__ == '__main__':
-    def_names = dir(hidden_4)
+    import hidden_4
 
-    for i in range(len(def_names)):
-        if def_names[i][:2] != '__':
-            print(def_names[i])
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print("{:s}".format(name))
