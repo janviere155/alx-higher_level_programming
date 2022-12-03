f38c01fe254f96aed974fcb64a3de75e0f006ad6
janviere155 <janviere155@gmail.com>
2022-12-03 10:15:24 -0800

    making files executable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -13,11 +13,11 @@ if __name__ == '__main__':
 
     if l_av > 1:
         print(l_av, 'arguments:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-        elif l_av == 1:
-            print(l_av, 'argument:')
-            for i in range(1, l_av + 1):
-                print('{:d}: {}'.format(i, av[i]))
-            elif l_av == 0:
-                print(l_av, 'arguments.')
+    for i in range(1, l_av + 1):
+        print('{:d}: {}'.format(i, av[i]))
+    elif l_av == 1:
+        print(l_av, 'argument:')
+    for i in range(1, l_av + 1):
+        print('{:d}: {}'.format(i, av[i]))
+    elif l_av == 0:
+        print(l_av, 'arguments.')
