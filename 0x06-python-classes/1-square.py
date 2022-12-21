1626d300d769955bacd0d08c20a31598cebad0ac
janviere155 <janviere155@gmail.com>
2022-12-21 02:03:39 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Module of class Square that defines the size of the square
+"""
+
+
+class Square:
+    """
+    Definition of the class Square
+    """
+    def __init__(self, size):
+        """
+        Initialize the attributes of the ``Square`` class.
+
+        Args:
+             size (int): first parameter. Defines the size of the Square
+        """
+       self.__size = size 
